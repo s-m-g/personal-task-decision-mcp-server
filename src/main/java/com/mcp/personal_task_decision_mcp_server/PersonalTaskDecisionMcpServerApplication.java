99ec71489cceb4d612e3302bb33450dc8d6e19bc
Shrey Mohan Gupta <shreymohan98@gmail.com>
2026-02-07 18:58:53 +0530

    phase-1: base setup for mcp server

---
+++ src/main/java/com/mcp/personal_task_decision_mcp_server/PersonalTaskDecisionMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.mcp.personal_task_decision_mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTaskDecisionMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTaskDecisionMcpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcp/personal_task_decision_mcp_server/PersonalTaskDecisionMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcp.personal_task_decision_mcp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTaskDecisionMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
